48b082e42901d3fbd5ec3b815519b0c85847fe94
Daniel Jackson <dajack05@gmail.com>
2022-10-13 21:44:09 -0400

    update

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts">import { ref } from 'vue';
+<script setup lang="ts">
 
+import { ref } from 'vue';
 
 const email = ref("");
 const password = ref("");
 
 function login(){
-    alert("Oh, hi Mark!\n\n"+email.value+" "+password.value);
+    
 }
 
 </script>
@@ -15,8 +16,8 @@ function login(){
         <div class="border shadow-lg m-auto p-10">
             <h1 class="text-4xl">Login</h1>
             <form @submit.prevent="login" class="border-t mt-2 pt-2 flex flex-col gap-2">
-                <input :value="email" type="email" placeholder="Email"/>
-                <input :value="password" type="password" placeholder="Password"/>
+                <input v-model="email" type="email" placeholder="Email"/>
+                <input v-model="password" type="password" placeholder="Password"/>
                 <button class="btn success">Login</button>
             </form>
         </div>
