14eece6ef1309031f2815851d2ce50c12fda55b0
Daniel Jackson <dajack05@gmail.com>
2022-12-06 22:56:59 -0500

    update

--- client/src/services/EmailService.ts
+++ client/src/services/EmailService.ts
@@ -1,14 +1,55 @@
-import axios from 'axios';
+import { useUserStore } from "@/stores/user";
+import type { Message } from "@ovc/common";
+import axios from "axios";
 
 const SERVER = import.meta.env.VITE_SERVER_URL;
 
 export class EmailService {
-  static async SendTestEmail(address:string): Promise<string|undefined>{
-    await axios.get(`${SERVER}/mail/send`,{
-      params:{
-        to:address,
+  static async SendText(
+    text: string,
+    subject: string,
+    to: string
+  ): Promise<string | undefined> {
+    const userStore = useUserStore();
+
+    try {
+      const response = await axios.post(`${SERVER}/mail/custom`, {
+        token: userStore.token,
+        subject: subject,
+        text: text,
+        email: to,
+      });
+
+      const msg = response.data as Message<unknown>;
+      if (msg.error) {
+        return msg.error;
       }
-    });
-    return "Sorry Mario, the princess is in another castle.";
+    } catch (err: unknown) {
+      return err as string;
+    }
+  }
+
+  static async SendHTML(
+    html: string,
+    subject: string,
+    to: string
+  ): Promise<string | undefined> {
+    const userStore = useUserStore();
+
+    try {
+      const response = await axios.post(`${SERVER}/mail/custom`, {
+        token: userStore.token,
+        subject: subject,
+        html: html,
+        email: to,
+      });
+
+      const msg = response.data as Message<unknown>;
+      if (msg.error) {
+        return msg.error;
+      }
+    } catch (err: unknown) {
+      return err as string;
+    }
   }
 }
--- client/src/views/EmailView.vue
+++ client/src/views/EmailView.vue
@@ -1,22 +1,74 @@
 <script setup lang="ts">
+import { EmailService } from '@/services/EmailService';
 import { UserService } from '@/services/UserService';
-import { computed } from '@vue/reactivity';
+import type { User } from '@ovc/common';
+import { onMounted, ref } from 'vue';
 
-const all_users = computed(() => UserService.GetAll());
+const all_users = ref<User[]>([]);
 
-function submit() {
-    alert("Bam!");
+const selected_user_email = ref<string>("");
+const text_content = ref<string>("");
+const subject = ref<string>("");
+
+const is_text_mode = ref<boolean>(true);
+const error_msg = ref<string>("");
+
+onMounted(async () => {
+    const users = await UserService.GetAll();
+    if (Array.isArray(users)) {
+        all_users.value = users;
+    }
+});
+
+async function submit() {
+    error_msg.value = "";
+    const error = await (async (): Promise<string | undefined> => {
+        if (is_text_mode.value) {
+            return await EmailService.SendText(text_content.value, subject.value, selected_user_email.value);
+        } else {
+            return await EmailService.SendHTML(text_content.value, subject.value, selected_user_email.value);
+        }
+        return undefined;
+    })();
+    if (error) {
+        error_msg.value = error;
+    } else {
+        error_msg.value = "All GOod!";
+    }
 }
 </script>
 <template>
     <div class="flex justify-center items-center">
         <div class="p-4 rounded border">
             <h1 class="text-4xl">Email</h1>
+            <p class="text-red-500 font-bold">{{ error_msg }}</p>
             <form @submit.prevent="submit">
                 <div>
-                    <select v-if="(typeof (all_users) !== 'string')">
-                        <option v-for="user, i in all_users" :key="i" :value="user.email" </option>
+                    <label>To</label>
+                    <select v-model="selected_user_email" required>
+                        <option v-for="user, i in all_users" :key="i" :value="user.email">{{ user.first_name }}
+                            {{ user.last_name }}</option>
                     </select>
+                </div>
+
+                <div>
+                    <label>Subject</label>
+                    <input type="text" v-model="subject" requried />
+                </div>
+
+                <div>
+                    <div class="flex justify-between items-baseline">
+                        <label>Message</label>
+                        <div>
+                            <button type="button" @click="(is_text_mode = true)" class="btn"
+                                :class="{ active: is_text_mode }">Text</button>
+                            <button type="button" @click="(is_text_mode = false)" class="btn"
+                                :class="{ active: !is_text_mode }">HTML</button>
+                        </div>
+                    </div>
+                    <textarea class="transition-colors"
+                        :class="{'bg-green-50':is_text_mode, 'bg-orange-50':!is_text_mode}" v-model="text_content"
+                        cols="64" rows="16" required></textarea>
                 </div>
 
                 <input type="submit" class="btn" value="Send it!" />
