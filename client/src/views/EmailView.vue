ebf69329c9b1ac614b92aeed3742b0e94408594c
Daniel Jackson <dajack05@gmail.com>
2022-12-08 07:43:33 -0500

    update

--- client/src/views/EmailView.vue
+++ client/src/views/EmailView.vue
@@ -6,12 +6,15 @@ import { onMounted, ref } from 'vue';
 
 const all_users = ref<User[]>([]);
 
-const selected_user = ref<User>();
+const selected_user = ref<User[]>([]);
 const text_content = ref<string>("");
 const subject = ref<string>("");
 
 const is_text_mode = ref<boolean>(true);
 const error_msg = ref<string>("");
+
+const sent_count = ref(0);
+const total_count = ref(0);
 
 onMounted(async () => {
     const users = await UserService.GetAll();
@@ -20,29 +23,43 @@ onMounted(async () => {
     }
 });
 
-function parse(input:string, user:User) {
-    return eval(input);
+function parseStringTemplate(str: string, obj: any) {
+    let parts = str.split(/\$\{(?!\d)[\wæøåÆØÅ]*\}/);
+    let args = str.match(/[^{\}]+(?=})/g) || [];
+    let parameters = args.map(argument => obj[argument] || (obj[argument] === undefined ? "" : obj[argument]));
+    return String.raw({ raw: parts }, ...parameters);
 }
 
 async function submit() {
     error_msg.value = "";
     const error = await (async (): Promise<string | undefined> => {
 
-        // Replace drop-ins
-
-        if(!selected_user.value){
+        if (!selected_user.value) {
             return "No User Selected";
         }
-        if (is_text_mode.value) {
-            return await EmailService.SendText(text_content.value, subject.value, selected_user.value.email);
-        } else {
-            return await EmailService.SendHTML(text_content.value, subject.value, selected_user.value.email);
+
+        total_count.value = selected_user.value.length;
+        sent_count.value = 0;
+
+        for (const user of selected_user.value) {
+            // Replace drop-ins
+            const filled_content = parseStringTemplate(text_content.value, { ...user });
+
+            if (is_text_mode.value) {
+                await EmailService.SendText(filled_content, subject.value, user.email);
+            } else {
+                await EmailService.SendHTML(filled_content, subject.value, user.email);
+            }
+
+            sent_count.value++;
         }
+
+        total_count.value = 0;
     })();
     if (error) {
         error_msg.value = error;
     } else {
-        error_msg.value = "All GOod!";
+        error_msg.value = "";
     }
 }
 </script>
@@ -51,39 +68,49 @@ async function submit() {
         <div class="p-4 rounded border">
             <h1 class="text-4xl">Email</h1>
             <p class="text-red-500 font-bold">{{ error_msg }}</p>
-            <form @submit.prevent="submit">
-                <div>
-                    <label>To</label>
-                    <select v-model="selected_user" required>
-                        <option v-for="user, i in all_users" :key="i" :value="user">{{ user.first_name }}
-                            {{ user.last_name }}</option>
-                    </select>
-                </div>
+            <form class="flex flex-col gap-2" @submit.prevent="submit">
+                <div class="flex gap-2">
+                    <div>
+                        <label>To</label>
+                        <select class="h-full w-fit" v-model="selected_user" multiple required>
+                            <option v-for="user, i in all_users" :key="i" :value="user">{{ user.first_name }}
+                                {{ user.last_name }}</option>
+                        </select>
+                    </div>
 
-                <div>
-                    <label>Subject</label>
-                    <input type="text" v-model="subject" requried />
-                </div>
-
-                <div>
-                    <div class="flex justify-between items-baseline">
-                        <label>Message</label>
+                    <div>
                         <div>
-                            <button type="button" @click="(is_text_mode = true)" class="btn"
-                                :class="{ active: is_text_mode }">Text</button>
-                            <button type="button" @click="(is_text_mode = false)" class="btn"
-                                :class="{ active: !is_text_mode }">HTML</button>
+                            <label>Subject</label>
+                            <input type="text" v-model="subject" requried />
+                        </div>
+
+                        <div>
+                            <div class="flex justify-between items-baseline">
+                                <label>Message</label>
+                                <div>
+                                    <button type="button" @click="(is_text_mode = true)" class="btn"
+                                        :class="{ active: is_text_mode }">Text</button>
+                                    <button type="button" @click="(is_text_mode = false)" class="btn"
+                                        :class="{ active: !is_text_mode }">HTML</button>
+                                </div>
+                            </div>
+                            <textarea class="transition-colors"
+                                :class="{ 'bg-green-50': is_text_mode, 'bg-orange-50': !is_text_mode }"
+                                v-model="text_content" cols="64" rows="16" required></textarea>
                         </div>
                     </div>
-                    <textarea class="transition-colors"
-                        :class="{ 'bg-green-50': is_text_mode, 'bg-orange-50': !is_text_mode }" v-model="text_content"
-                        cols="64" rows="16" required></textarea>
                 </div>
-
-                <button @click="parse(text_content, selected_user)" class="btn" type="button">Parse</button>
 
                 <input type="submit" class="btn" value="Send it!" />
             </form>
+            <div v-if="(total_count > 0)"
+                class="bg-[#00000088] absolute top-0 left-0 w-full h-full flex justify-center items-center">
+                <div class="bg-white rounded border p-4 flex gap-2 flex-col justify-center text-center">
+                    <p class="text-2xl">Please wait while your emails are sent.</p>
+                    <i>If you close this window, not all emails will be sent.</i>
+                    <label>{{ sent_count }}/{{ total_count }} {{ Math.round((sent_count / total_count) * 100) }}%</label>
+                </div>
+            </div>
         </div>
     </div>
 </template>
