278a8a98891e49cd9429584a92b488d955814bf2
Daniel Jackson <dajack05@gmail.com>
2022-12-08 20:34:40 -0500

    update

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -12,18 +12,18 @@ const userStore = useUserStore();
 
 const allUsers = ref<User[]>([]);
 
-onMounted(async ()=>{
-  if(!userStore.isLoggedIn()){
+onMounted(async () => {
+  if (!userStore.isLoggedIn()) {
     useRouter().push("/login");
   }
   await getAll();
 })
 
-async function getAll(){
+async function getAll() {
   const _allUsers = await UserService.GetAll();
-  if(typeof(_allUsers) === 'string'){
+  if (typeof (_allUsers) === 'string') {
     console.error(_allUsers);
-  }else{
+  } else {
     allUsers.value = _allUsers;
   }
 }
@@ -31,13 +31,28 @@ async function getAll(){
 </script>
 
 <template>
-  <main class="text-center m-10">
-    <RouterLink v-if="!userStore.isLoggedIn()" class="btn text-2xl" to="/login">Login</RouterLink>
-    <div class="flex flex-wrap justify-around" v-if="userStore.isLoggedIn()">
-      <!-- Logged In Info -->
-      <UserViewer />
-      <GroupViewer />
-      <EmailControlPanel />
-    </div>
-  </main>
+  <div class="container m-auto border rounded">
+    <p class="text-2xl font-semibold m-2">Member Dashboard</p>
+    <hr />
+    <div class="max-h-[200px] overflow-y-auto">
+    <table class="">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>DOB</th>
+          <th>BFR Due</th>
+          <th>Medical Due</th>
+        </tr>
+      </thead>
+      <tbody class="overflow-y-auto">
+        <tr v-for="user, i in allUsers" :key="i">
+          <td class="px-4 py-2 border-b">{{ user.first_name }} {{ user.last_name }}</td>
+          <td class="px-4 py-2 border-b">{{ user.dob }}</td>
+          <td class="px-4 py-2 border-b">BFR Date</td>
+          <td class="px-4 py-2 border-b">Medical Due</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  </div>
 </template>
